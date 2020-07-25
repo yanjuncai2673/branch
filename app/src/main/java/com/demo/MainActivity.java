f1d54b84a91818a7b8ba43e444b17c0e9184cf2a
yanjuncai2673 <1617403589@qq.com>
2020-07-25 14:42:35 +0800

    上传

---
+++ app/src/main/java/com/demo/MainActivity.java
@@ -0,0 +1,31 @@
+package com.demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+    public class ListNode{
+
+
+
+
+
+
+
+
+
+    }
+
+}
